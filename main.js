f9ad2829f42f71abb6ed1179a9faa1d2301bfff7
Antônio Carlos <juniorbecari@gmail.com>
2022-01-15 17:05:37 -0300

    Added some things

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ function createWindow() {
     });
     
     win.loadFile("index.html");
+    //win.removeMenu();
 }
 
 app.whenReady().then(() => {
--- renderer.js
+++ renderer.js
@@ -1,1 +1,32 @@
+const refreshBtn = document.getElementById("refresh-btn");
+const addressBar = document.getElementById("address-bar");
+const webview = document.getElementById("webview");
 
+const enter = 13;
+
+window.onkeydown = (e) => {
+    if (e.keyCode === enter) {
+        let url = addressBar.value;
+        
+        if (url === "") return;
+        
+        if (!(url.startsWith("http://") || url.startsWith("https://"))) {
+            url = "https://" + url;
+        }
+        
+        webview.src = url;
+        addressBar.value = url;
+        console.log("loading");
+        webview.loadUrl(url);
+    }
+};
+
+webview.addEventListener("did-start-loading", () => {
+    refreshBtn.classList.add("rotate");
+    console.log("started");
+});
+
+webview.addEventListener("did-stop-loading", () => {
+    refreshBtn.classList.remove("rotate");
+    console.log("stopped");
+});
